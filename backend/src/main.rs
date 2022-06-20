125ac12df188c2c615062a9825530feda52d61cc
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-06-20 19:58:44 +0200

    made login endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,26 @@
+use actix_cors::Cors;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-struct Greeting {
-    a: u64,
-    b: String,
+struct Credentials {
+    username: String,
+    passwd: String,
 }
 
-async fn hello(greeting: web::Json<Greeting>) -> impl Responder {
-    println!("{:?}", greeting);
+async fn login(credentials: web::Json<Credentials>) -> impl Responder {
+    println!("{:?}", credentials);
     HttpResponse::Ok().body("Yeehaw\n")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/hello", web::post().to(hello)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Cors::permissive())
+            .service(web::scope("/api/v1").route("/login", web::post().to(login)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
