33bf2adce8d04d5fe39a7685d71f938105da955a
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-06-20 17:09:20 +0200

    added login form

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,23 +1,29 @@
-import { CSSProperties, useState } from "react";
+import { ChangeEvent, CSSProperties, useState } from "react";
 import "./Input.scss";
 
 interface InputProps {
     placeholder?: string,
+    type?: "text" | "password"
     style?: CSSProperties,
+    onChanged: (e: ChangeEvent<HTMLInputElement>) => void,
 }
 
 function Input(props: InputProps) {
     let [accessed, setAccessed] = useState(false);
+    let { type = "text" } = props;
 
     return (
         <div className="container" style={props.style}>
             {props.placeholder ? <span className={"placeholder" + (accessed ? " placeholder-moved" : "")}>{props.placeholder}</span> : null}
-            <input className="input-field"
+            <input type={type} className="input-field"
                 onFocus={() => {
                     setAccessed(true);
-                }} onBlur={(e) => {
+                }}
+                onBlur={(e) => {
                     setAccessed(!!e.target.value);
-                }} />
+                }}
+                onChange={props.onChanged}
+            />
         </div>
     );
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,19 +2,30 @@ import "./Login.scss";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import { Link } from "wouter";
+import { useState } from "react";
 
 function Login() {
-    function login(e: any) { }
+    let [username, setUsername] = useState("");
+    let [password, setPassword] = useState("");
+
+    function login(e: any) {
+        console.log(username, password)
+    }
 
     return (
-        <div>
-            Login
-            <Input placeholder="Username" />
-            <div className="login-buttons">
-                <Link to="/register">
-                    <Button onClick={() => { }} inverted={true} style={{ width: "120px" }}>Register</Button>
-                </Link>
-                <Button onClick={login} style={{ width: "120px" }}>Login</Button>
+        <div className="login-container">
+            <div className="login-content">
+                <span className="login-title">Login</span>
+                <div className="login-form">
+                    <Input placeholder="Username" type="text" onChanged={(e) => { setUsername(e.target.value) }} />
+                    <Input placeholder="Password" type="password" onChanged={(e) => { setPassword(e.target.value) }} />
+                    <div className="login-buttons">
+                        <Link to="/register">
+                            <Button onClick={() => { }} inverted={true} style={{ width: "120px" }}>Register</Button>
+                        </Link>
+                        <Button onClick={login} style={{ width: "120px" }}>Login</Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
