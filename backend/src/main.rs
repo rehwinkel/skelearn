0a950bfac2b3ab6a7ab9a6453a5ac8668ce56697
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-06-21 18:54:54 +0200

    added mongo

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,18 @@
 use actix_cors::Cors;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use mongodb::{options::ClientOptions, Client, Collection};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug)]
 struct Credentials {
     username: String,
     passwd: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    username: String,
+    passwd_hash: String,
 }
 
 async fn login(credentials: web::Json<Credentials>) -> impl Responder {
@@ -14,7 +21,21 @@ async fn login(credentials: web::Json<Credentials>) -> impl Responder {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> eyre::Result<()> {
+    let client_opts = ClientOptions::parse("mongodb://127.0.0.1:27017").await?;
+    let client = Client::with_options(client_opts)?;
+    let db = client.database("skelearn");
+    let users: Collection<User> = db.collection("users");
+    users
+        .insert_one(
+            User {
+                username: String::from("ian"),
+                passwd_hash: bcrypt::hash("123", bcrypt::DEFAULT_COST)?,
+            },
+            None,
+        )
+        .await?;
+
     HttpServer::new(|| {
         App::new()
             .wrap(Cors::permissive())
@@ -22,5 +43,6 @@ async fn main() -> std::io::Result<()> {
     })
     .bind(("127.0.0.1", 8080))?
     .run()
-    .await
+    .await?;
+    Ok(())
 }
