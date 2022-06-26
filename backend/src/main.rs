667b2737e4901af1788742eebdbe45890ac85e9b
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-06-26 13:04:03 +0200

    made backend pass mongodb data

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,9 @@
 use actix_cors::Cors;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use mongodb::{options::ClientOptions, Client, Collection};
+use actix_web::{
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder, Result,
+};
+use mongodb::{options::ClientOptions, Client, Collection, Database};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug)]
@@ -15,17 +18,10 @@ struct User {
     passwd_hash: String,
 }
 
-async fn login(credentials: web::Json<Credentials>) -> impl Responder {
-    println!("{:?}", credentials);
-    HttpResponse::Ok().body("Yeehaw\n")
-}
-
-#[actix_web::main]
-async fn main() -> eyre::Result<()> {
-    let client_opts = ClientOptions::parse("mongodb://127.0.0.1:27017").await?;
-    let client = Client::with_options(client_opts)?;
-    let db = client.database("skelearn");
+async fn login(db: &Database, creds: &Credentials) -> eyre::Result<()> {
     let users: Collection<User> = db.collection("users");
+    println!("{:?}", creds);
+    // users.find_one(filter, options)
     users
         .insert_one(
             User {
@@ -35,11 +31,32 @@ async fn main() -> eyre::Result<()> {
             None,
         )
         .await?;
+    Ok(())
+}
 
-    HttpServer::new(|| {
+async fn login_handler(db: Data<Database>, credentials: web::Json<Credentials>) -> HttpResponse {
+    println!("wrhwhwrh");
+    match login(&db, &credentials).await {
+        Ok(_) => HttpResponse::Ok().body("1816351093"),
+        Err(e) => {
+            eprintln!("Error while handling login: {}", e);
+            HttpResponse::InternalServerError().body("Internal Server Error")
+        }
+    }
+}
+
+#[actix_web::main]
+async fn main() -> eyre::Result<()> {
+    let client_opts = ClientOptions::parse("mongodb://127.0.0.1:27017").await?;
+    let client = Client::with_options(client_opts)?;
+    let db = client.database("skelearn");
+    let db_data = Data::new(db);
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(db_data.clone())
             .wrap(Cors::permissive())
-            .service(web::scope("/api/v1").route("/login", web::post().to(login)))
+            .service(web::scope("/api/v1").route("/login", web::post().to(login_handler)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
