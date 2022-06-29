ec9da5b249ad85f8d593776b4790702b2cb7a56a
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-06-29 17:57:31 +0200

    added right half of dashboard and check component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,8 +3,19 @@ import Card from "../components/Card";
 import Select from "../components/Select";
 import IconButton from "../components/IconButton";
 import { mdiPencil } from "@mdi/js";
+import Button from "../components/Button";
+import { useState } from "react";
+import Check from "../components/Check";
+
+enum TestMode {
+    SpacedRep,
+    Timed,
+    Test,
+}
 
 function Dashboard() {
+    let [mode, setMode] = useState<TestMode>(TestMode.SpacedRep);
+
     return (
         <div className="dashboard-container">
             <Card loading={false}>
@@ -21,6 +32,19 @@ function Dashboard() {
                     <Select options={["test", "yeag", "nice"]} onSelected={(e) => { console.log(e) }}></Select>
                     <IconButton icon={mdiPencil} onClick={() => { }}></IconButton>
                 </div>
+                <span className="dashboard-section-title">Lernmodus</span>
+                <Button size="large" onClick={() => { }}>Jetzt loslernen</Button>
+                <span className="dashboard-section-title">Abfragemodus</span>
+                <div className="dashboard-test-settings">
+                    <div className="dashboard-mode-buttons">
+                        <Button color="accent" inverted={mode !== TestMode.SpacedRep} onClick={() => { setMode(TestMode.SpacedRep) }}>Spaced-Rep.</Button>
+                        <Button color="accent" inverted={mode !== TestMode.Timed} onClick={() => { setMode(TestMode.Timed) }}>Timer</Button>
+                        <Button color="accent" inverted={mode !== TestMode.Test} onClick={() => { setMode(TestMode.Test) }}>Prüfung</Button>
+                    </div>
+                    <Check onToggle={() => { }}>Knochen benennen</Check>
+                    <Check onToggle={() => { }}>Knochen identifizieren</Check>
+                </div>
+                <Button style={{ marginTop: "16px" }} size="large" onClick={() => { }}>Jetzt abfragen</Button>
             </Card>
         </div>
     );
