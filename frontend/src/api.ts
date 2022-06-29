82a25f9f94303e522a1ff7090f4652caed00f973
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-06-29 16:18:25 +0200

    moved hash to extra func

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -8,15 +8,19 @@ function get(path: string, headers: any): Promise<Response> {
     return fetch(endpoint + path, { method: "GET", mode: "cors", headers: { "Content-Type": "application/json", ...headers } });
 }
 
-async function apiLogin(username: string, passwd: string): Promise<Response | null> {
+async function hashPasswd(passwd: string): Promise<string> {
     const encoder = new TextEncoder();
     const data = encoder.encode(passwd);
     const hashBuffer = await crypto.subtle.digest('SHA-256', data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+async function apiLogin(username: string, passwd: string): Promise<Response | null> {
     let response: Response;
     try {
-        response = await post("/login", {}, { username, passwd: hashHex });
+        response = await post("/login", {}, { username, passwd: await hashPasswd(passwd) });
         return response;
     } catch (err: any) {
         console.error(err);
