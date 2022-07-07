280a7918f8ec209b3a8dd4586b30ed5c57d33ee2
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-07-07 13:17:16 +0200

    merged and removed 2nd test logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,12 +12,9 @@ import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import { AuthProvider, RequireAuth, useAuth } from './auth';
 import Learn from './pages/Learn';
-<<<<<<< HEAD
-=======
 import RegularExam from './pages/RegularExam';
 import RealExam from './pages/RealExam';
 import Impressum from './pages/Impressum';
->>>>>>> 5d672037c48d928d9d0850717785f241304725cc
 import Categories from './pages/Categories';
 
 function HomeOrLogin() {
@@ -52,11 +49,6 @@ function App() {
                         <Route path="/login" ><Login /></Route>
                         <Route path="/register" ><Register /></Route>
                         <Route path="/dashboard" ><RequireAuth fallback={<Redirect to="/login" />}><Dashboard /></RequireAuth></Route>
-<<<<<<< HEAD
-                        <Route path="/learn" ><RequireAuth fallback={<Redirect to="/login" />}><Learn /></RequireAuth></Route>
-                        <Route path="/categories"><Categories /></Route>
-                        <Route path="/:rest"><Redirect to="/" /></Route>
-=======
                         <Route path="/categories" ><RequireAuth fallback={<Redirect to="/login" />}><Categories /></RequireAuth></Route>
                         <Route path="/learn/:category" >
                             {(params) => {
@@ -85,7 +77,6 @@ function App() {
                             }}
                         </Route>
                         <Route path="/:rest*"><Redirect to="/" /></Route>
->>>>>>> 5d672037c48d928d9d0850717785f241304725cc
                     </Switch>
                 </div>
                 <div style={{ flexGrow: location === "/" ? 0 : 1 }}></div>
